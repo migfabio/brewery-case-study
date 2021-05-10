5c6f08332305fd802a28549fc46a2b0459cf1d28
Fabio Mignogna <fmignogna@deem.com>
2021-05-10 10:32:12 +0100

    Initial Commit

---
+++ BreweryCore/BreweryCore.h
@@ -0,0 +1,18 @@
+//
+//  BreweryCore.h
+//  BreweryCore
+//
+//  Created by Fabio Mignogna on 10/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BreweryCore.
+FOUNDATION_EXPORT double BreweryCoreVersionNumber;
+
+//! Project version string for BreweryCore.
+FOUNDATION_EXPORT const unsigned char BreweryCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BreweryCore/PublicHeader.h>
+
+
