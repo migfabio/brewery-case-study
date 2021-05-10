33c9a49d9d69bfd8887166b57e3c813caef17ec7
Fabio Mignogna <fmignogna@deem.com>
2021-05-10 11:05:56 +0100

    Brewery Remote Loader requests data from URL

--- BreweryCoreTests/API/BreweryRemoteLoaderTests.swift
+++ BreweryCoreTests/API/BreweryRemoteLoaderTests.swift
@@ -1,20 +1,42 @@
 import XCTest
 
 final class BreweryRemoteLoader {
-    init(httpClient: HTTPClientSpy) {
+    private let httpClient: HTTPClientSpy
+    private let url: URL
 
+    init(httpClient: HTTPClientSpy, url: URL) {
+        self.httpClient = httpClient
+        self.url = url
+    }
+
+    func load() {
+        httpClient.get(from: url)
     }
 }
 
 final class HTTPClientSpy {
     private(set) var requestedURL: URL?
+
+    func get(from url: URL) {
+        self.requestedURL = url
+    }
 }
 
 final class BreweryRemoteLoaderTests: XCTestCase {
 
     func test_init_shouldNotRequestDataFromURL() {
         let httpClient = HTTPClientSpy()
-        _ = BreweryRemoteLoader(httpClient: httpClient)
+        _ = BreweryRemoteLoader(httpClient: httpClient, url: URL(string: "https://random.com")!)
         XCTAssertNil(httpClient.requestedURL)
+    }
+
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://any-url.com")!
+        let httpClient = HTTPClientSpy()
+        let sut = BreweryRemoteLoader(httpClient: httpClient, url: url)
+
+        sut.load()
+
+        XCTAssertEqual(httpClient.requestedURL, url)
     }
 }
