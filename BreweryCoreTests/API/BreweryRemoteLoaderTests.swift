6f9e55fa2b4e0bb59a6250d0a7953a57a2ecaa40
Fabio Mignogna <fmignogna@deem.com>
2021-05-10 15:26:14 +0100

    Add memory leak tracking

--- BreweryCoreTests/API/BreweryRemoteLoaderTests.swift
+++ BreweryCoreTests/API/BreweryRemoteLoaderTests.swift
@@ -42,9 +42,13 @@ final class BreweryRemoteLoaderTests: XCTestCase {
         XCTAssertEqual(httpClient.requestedURL, url)
     }
 
-    private func makeSUT(url: URL = URL(string: "https://given-url.com")!) -> (sut: BreweryRemoteLoader, httpClient: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://given-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: BreweryRemoteLoader, httpClient: HTTPClientSpy) {
         let httpClient = HTTPClientSpy()
         let sut = BreweryRemoteLoader(httpClient: httpClient, url: url)
+        addTeardownBlock { [weak httpClient, weak sut] in
+            XCTAssertNil(httpClient, "HTTP Client is not deallocated. Potential memory leak.", file: file, line: line)
+            XCTAssertNil(sut, "Brewery Remote Loader is not deallocated. Potential memory leak.", file: file, line: line)
+        }
         return (sut, httpClient)
     }
 }
