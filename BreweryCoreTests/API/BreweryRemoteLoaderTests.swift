68aec7018ea5707e5b5a3d703fee7b8afc342b41
Fabio Mignogna <fmignogna@deem.com>
2021-05-10 15:46:29 +0100

    Brewery Remote Loader returns error on client error

--- BreweryCoreTests/API/BreweryRemoteLoaderTests.swift
+++ BreweryCoreTests/API/BreweryRemoteLoaderTests.swift
@@ -1,28 +1,42 @@
 import XCTest
 
 protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Result<Void, Error>) -> Void)
 }
 
 final class BreweryRemoteLoader {
     private let httpClient: HTTPClient
     private let url: URL
 
+    enum Error: Swift.Error, Equatable {
+        case clientError
+    }
+
     init(httpClient: HTTPClient, url: URL) {
         self.httpClient = httpClient
         self.url = url
     }
 
-    func load() {
-        httpClient.get(from: url)
+    func load(completion: @escaping (Result<Void, Error>) -> Void) {
+        httpClient.get(from: url) { _ in
+            completion(.failure(.clientError))
+        }
     }
 }
 
 final class HTTPClientSpy: HTTPClient {
-    private(set) var requestedURLs: [URL] = []
+    private var requests = [(url: URL, completion: (Result<Void, Error>) -> Void)]()
 
-    func get(from url: URL) {
-        self.requestedURLs.append(url)
+    var requestedURLs: [URL] {
+        requests.map { $0.url }
+    }
+    
+    func get(from url: URL, completion: @escaping (Result<Void, Error>) -> Void) {
+        self.requests.append((url, completion))
+    }
+
+    func completeWithError(at index: Int) {
+        requests[index].completion(.failure(NSError(domain: "", code: 0)))
     }
 }
 
@@ -37,7 +51,7 @@ final class BreweryRemoteLoaderTests: XCTestCase {
         let url = URL(string: "https://any-url.com")!
         let (sut, httpClient) = makeSUT(url: url)
 
-        sut.load()
+        sut.load { _ in }
 
         XCTAssertEqual(httpClient.requestedURLs, [url])
     }
@@ -46,11 +60,33 @@ final class BreweryRemoteLoaderTests: XCTestCase {
         let url = URL(string: "https://any-url.com")!
         let (sut, httpClient) = makeSUT(url: url)
 
-        sut.load()
-        sut.load()
+        sut.load { _ in }
+        sut.load { _ in }
 
         XCTAssertEqual(httpClient.requestedURLs, [url, url])
     }
+
+    func test_load_returnsErrorOnClientError() {
+        let (sut, httpClient) = makeSUT()
+
+        let exp = expectation(description: "Waiting for load to finish")
+
+        sut.load { result in
+            switch result {
+            case .failure(let error):
+                XCTAssertEqual(error, .clientError)
+            case .success:
+                XCTFail("Got \(result) instead of failure")
+            }
+            exp.fulfill()
+        }
+
+        httpClient.completeWithError(at: 0)
+
+        wait(for: [exp], timeout: 1)
+    }
+
+    // MARK: - Test Helpers
 
     private func makeSUT(url: URL = URL(string: "https://given-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: BreweryRemoteLoader, httpClient: HTTPClientSpy) {
         let httpClient = HTTPClientSpy()
