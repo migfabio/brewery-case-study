20c530d6d2a8d9649a1e2dce6c36f4fb2a55111a
Christopher La Pat <clapat@deem.com>
2021-05-14 11:30:08 +0100

    Fail on invalid representation

--- BreweryCoreTests/API/URLSessionHTTPClientTests.swift
+++ BreweryCoreTests/API/URLSessionHTTPClientTests.swift
@@ -2,6 +2,8 @@ import BreweryCore
 import XCTest
 
 class URLSessionHTTPClient: HTTPClient {
+    private struct InvalidRepresentation: Error {}
+    
     private let session: URLSession
 
     init(session: URLSession = .shared) {
@@ -12,13 +14,15 @@ class URLSessionHTTPClient: HTTPClient {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(InvalidRepresentation()))
             }
         }.resume()
     }
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
-    func test_get_failsOnRequestsError() {
+    func test_get_failsOnRequestError() {
         URLProtocol.registerClass(URLProtocolStub.self)
         let url = URL(string: "https://any-url.com")!
         let expectedError = NSError(domain: "", code: 0)
@@ -42,11 +46,40 @@ final class URLSessionHTTPClientTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         URLProtocolStub.unregisterClass(URLProtocolStub.self)
     }
+    
+    func test_get_failsOnInvalidStates() {
+        URLProtocol.registerClass(URLProtocolStub.self)
+        let url = URL(string: "https://any-url.com")!
+        
+        URLProtocolStub.data = nil
+        URLProtocolStub.response = nil
+        URLProtocolStub.error = nil
+        
+
+        let sut = URLSessionHTTPClient()
+
+        let exp = expectation(description: "Waiting for completion")
+
+        sut.get(from: url) { result in
+            switch result {
+            case let .failure(receivedError):
+                XCTAssertNotNil(receivedError)
+            default:
+                XCTFail("Expected failure, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.unregisterClass(URLProtocolStub.self)
+    }
 }
 
 private extension URLSessionHTTPClientTests {
 
     final class URLProtocolStub: URLProtocol {
+        static var data: Data?
+        static var response: URLResponse?
         static var error: Error?
 
         override class func canInit(with request: URLRequest) -> Bool {
@@ -58,6 +91,14 @@ private extension URLSessionHTTPClientTests {
         }
 
         override func startLoading() {
+            if let data = URLProtocolStub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = URLProtocolStub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
             if let error = URLProtocolStub.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
